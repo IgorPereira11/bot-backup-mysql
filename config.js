17dece1bdc8d7895184e1913794960b7d515eae0
IgorPereira11 <igorsantos1144@gmail.com>
2023-06-27 13:55:03 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+    webhook: "SUA WEBHOOK AQUI",
+    time: 1, // in min
+
+    connection: {
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        database: "vrp",
+    },
+
+    embed: {
+        color: "BLACK"
+    }
+}
+
+module.exports = config
